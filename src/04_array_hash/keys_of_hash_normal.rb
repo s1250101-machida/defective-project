6e039f06fac92a2c67afd74f5f4ab85868d80c9c
sXX1250101-machida <s1250101@u-aizu.ac.jp>
2017-07-20 18:25:18 +0900

    fix 04_array_hash/orthogonal_vectors_easy.rb

--- src/04_array_hash/keys_of_hash_normal.rb
+++ src/04_array_hash/keys_of_hash_normal.rb
@@ -2,3 +2,10 @@ def keys_of_hash(arr)
     # Please select only keys with value is even
     arr.select{|k, v| v % 2 == 0}.keys
 end
+
+def
+  puts
+actual
+expected
+assert_equal expected, actual
+assert_equal expexted
